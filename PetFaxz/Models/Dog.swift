0543367985553ceab90ee38a8211de686d38c4af
twof <fabiobean2@gmail.com>
2017-05-14 13:50:58 -0700

    Added dog model. Moved view controllers

---
+++ PetFaxz/Models/Dog.swift
@@ -0,0 +1,16 @@
+//
+//  Dog.swift
+//  PetFaxz
+//
+//  Created by fnord on 5/14/17.
+//  Copyright © 2017 twof. All rights reserved.
+//
+
+import Foundation
+
+public struct Dog {
+    var name: String
+    var pronouns: String
+    var age: Int
+    var breed: String
+}

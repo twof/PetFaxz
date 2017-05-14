2f6430b05291764284472445c3aa3e972a5daff2
twof <fabiobean2@gmail.com>
2017-05-13 20:23:09 -0700

    Initial commit

---
+++ PetFaxz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetFaxz
+//
+//  Created by fnord on 5/13/17.
+//  Copyright © 2017 twof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
